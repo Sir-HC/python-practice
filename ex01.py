8d5bd1479f6ae7773e19604bb95a268139e8839c
Chris Lynch <clynch@uoregon.edu>
2018-12-26 08:35:36 -0500

    ex01

---
+++ ex01.py
@@ -0,0 +1,18 @@
+name = input('give me your name: ')
+print('your name is ' + name)
+
+age = int(input("enter your age"))
+
+print('your age is ' + str(age))
+
+print('4' + 'cats')
+
+
+print(str(4) + 'cats')
+
+print(4* 'cats')
+input('divisor crash?')
+
+print(4/'cats')
+
+input('end?')
